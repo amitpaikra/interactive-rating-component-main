1cf2d809238fe83318511ca8caa936a90bdb2dea
Amit Paikra <paikramit1999@gmail.com>
2022-05-29 17:17:46 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const submitButton = document.getElementsByTagName("button")[0];
+
+submitButton.addEventListener("click", ()=>{
+    document.getElementsByClassName("container")[0].style.visibility = "hidden";
+
+});
+
